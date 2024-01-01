a86276379dd57815185e6f5dbb811f60e1ea6def
Le Hoang Trong <langtupro0456@gmail.com>
2024-01-01 10:27:59 +0700

    Khởi tạo base project Twitter API

---
+++ src/controllers/users.controllers.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express'
+import User from '~/models/schemas/User.schema'
+import databaseService from '~/services/database.services'
+import usersService from '~/services/users.services'
+
+export const loginController = (req: Request, res: Response) => {
+  const { email, password } = req.body
+  if (email === 'duthanhduoc@gmail.com' && password === '123456') {
+    return res.status(200).json({
+      message: 'Login success'
+    })
+  }
+  return res.status(400).json({
+    error: 'Login failed'
+  })
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  try {
+    const result = await usersService.register({ email, password })
+    // await databaseService.users.find({})
+    return res.json({
+      message: 'Register success',
+      result
+    })
+  } catch (error) {
+    return res.status(400).json({
+      message: 'Register failed',
+      error
+    })
+  }
+}
